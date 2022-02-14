bd62440a619902a3691343b33d1e6df0af387acd
Jonathan Goulding <58443816+jonathangoulding@users.noreply.github.com>
2022-02-14 08:47:46 +0000

    feat: run reset project in parallel

--- zedmund/reset-main.sh
+++ zedmund/reset-main.sh
@@ -2,16 +2,22 @@ source repos.sh
 
 cd ..
 
+reset_project() {
+      echo "#################### $1 ####################"
+      cd "$1" || exit
+      git stash
+      git checkout main
+      git fetch origin
+      git reset --hard origin/main
+      git clean -d -f
+      git pull
+      npm ci
+      cd - > /dev/null || exit
+}
+
 for repository in "${repositories[@]}"
 do
-    echo "#################### $repository ####################"
-    cd "$repository" || exit
-    git stash
-    git checkout main
-    git fetch origin
-    git reset --hard origin/main
-    git clean -d -f
-    git pull
-    npm ci
-    cd - > /dev/null
+  reset_project "$repository" &
 done
+
+wait
